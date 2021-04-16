43536a60f4f6e57b40dc55e7779106145745e807
luis.pires <luispires.m@gmail.com>
2021-04-16 17:52:54 -0300

    nesse momento eu posso declarar o tipo da variavel usando a forma onde ue coloco essa variavel e depois o tipo no caso String

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,8 @@
 fun main() {
-    //ao declarar uma variavel com val ocorre um erro na aplicação pois val é imutavel
-    //o erro é Val cannot be reassigned
-    val nome = "Luis"
-    println("Ola  "+nome)
-    nome = "Felipe"
-    println("Ola  "+nome)
-    nome = "Alex"
-    println("Ola  "+nome)
-    nome = "Gustavo"
-    println("Ola  "+nome)
+
+    // declarando o tipo da String
+    val nome = "Luis Mauricio Carabajal Pirea"
+    val Nome: String = " Luis mairicio "
+    println()
+
 }
