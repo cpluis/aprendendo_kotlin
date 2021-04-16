d8fd6ca90cefc14a0e026fc5577d3aa318379196
luis.pires <luispires.m@gmail.com>
2021-04-16 17:38:51 -0300

    como é possivel ver posso atribuir nomes na minha variavel de forma tranquila

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,4 +2,10 @@ fun main() {
     //variavel var é mutavel, isso é ela pode mudar
     var nome = "Luis"
     println("Ola  "+nome)
+    nome = "Felipe"
+    println("Ola  "+nome)
+    nome = "Alex"
+    println("Ola  "+nome)
+    nome = "Gustavo"
+    println("Ola  "+nome)
 }
