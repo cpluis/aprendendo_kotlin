08fbefcfe5e0e77fed82c4dc7ca72f5abea2cb32
luis.pires <luispires.m@gmail.com>
2021-04-16 17:42:49 -0300

    //ao declarar uma variavel com val ocorre um erro na aplicação pois val é imutavel     //o erro é Val cannot be reassigned

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,7 @@
 fun main() {
-    //variavel var é mutavel, isso é ela pode mudar
-    var nome = "Luis"
+    //ao declarar uma variavel com val ocorre um erro na aplicação pois val é imutavel
+    //o erro é Val cannot be reassigned
+    val nome = "Luis"
     println("Ola  "+nome)
     nome = "Felipe"
     println("Ola  "+nome)
