9af0d748ae6fb10393ae5496f2b48ccb6e797c29
luis.pires <luispires.m@gmail.com>
2021-04-16 18:15:20 -0300

    lembrando de usar ponto e não virgula para declarar decimais em kotlin.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,8 @@
 fun main() {
 
-    // declarando o tipo da String
-    val nome = "Luis Mauricio Carabajal Pirea"
-    val Nome: String = " Luis mairicio "
-    println()
-
+    // não se coloca virgula em numeros decimais na programação e sim é preciso colocar ponto
+    // como mostro no exemplo
+    val pagamento =10.00
+    val nome: String = " Luis mairicio "
+    println(nome+" seu pagamento pela aula de kotlin é  "+pagamento)
 }
