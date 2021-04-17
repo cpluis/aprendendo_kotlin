6c4dfb8f9c1d499e3c52382c4f012a257e11a293
luis.pires <luispires.m@gmail.com>
2021-04-16 22:27:12 -0300

    Tipos das variaveis

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,7 @@
 fun main() {
 
-    /**/
-    // não se coloca virgula em numeros decimais na programação e sim é preciso colocar ponto
-    // como mostro no exemplo
-    val pagamento =10.00  //entende com double
-    val nome: String = " Luis mairicio "
-    val valorFloat = 50.00f   //intende como Float
-    val valorLogico = true              //intende como boolean
-    val valorLong =
+   val  nome = "Luis Mauricio"
+    val idade = 40
+
+    println(" Meu nome é $nome e tenho $idade e sou de Porto Alegre!!!")
 }
